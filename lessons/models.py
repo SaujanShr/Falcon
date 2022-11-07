59b7a4b20e6bbce3a9449eb74cb3a26511650094
Shrestha, Saujan (k21075139) <saujan.shrestha@kcl.ac.uk>
2022-11-07 03:24:42 +0000

    Add tests for request model.

--- lessons/models.py
+++ lessons/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.core.validators import MinLengthValidator
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator
 
 class DayOfTheWeek(models.Model):
     day = models.CharField(max_length=10, editable=False)
@@ -10,6 +11,10 @@ class DayOfTheWeek(models.Model):
     def __str__(self):
         return self.day
 
+
+class User(AbstractUser):
+    # Add code here. I've added placeholder code in the meantime.
+    pass
 
 class Request(models.Model):
     class Day(models.TextChoices):
@@ -30,12 +35,9 @@ class Request(models.Model):
         FOURTY_FIVE_MINUTES = 45, '45 Minutes'
         SIXTY_MINUTES = 60, '60 Minutes'
 
-    student_id = models.CharField(
-            max_length = 4,
-            validators=[MinLengthValidator(4)]
-        )
-    availability = models.ManyToManyField(DayOfTheWeek)
-    number_of_lessons = models.PositiveIntegerField(choices=LessonDuration.choices)
+    user = models.ForeignKey(User, blank=False, on_delete=models.CASCADE)
+    availability = models.ManyToManyField(DayOfTheWeek, blank=False)
+    number_of_lessons = models.PositiveIntegerField(validators=[MinValueValidator(1)])
     interval_between_lessons = models.PositiveIntegerField(choices=IntervalBetweenLessons.choices)
     duration_of_lessons = models.PositiveIntegerField(choices=LessonDuration.choices)
-    further_information = models.CharField(max_length=500)
+    further_information = models.CharField(blank=False, max_length=500)
--- lessons/tests.py
+++ lessons/tests.py
@@ -1,2 +1,72 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
+from .models import User, Request
 
+class RequestModelTestCase(TestCase):
+    def setUp(self):
+        self.request = Request.objects.create(
+            user = User.objects.create_user(
+                username='username',
+                password='password'
+            ),
+            number_of_lessons = 1,
+            interval_between_lessons = Request.IntervalBetweenLessons.ONE_WEEK,
+            duration_of_lessons = Request.LessonDuration.THIRTY_MINUTES,
+            further_information = 'Some information...'
+        )
+        self.request.availability.create(day = Request.Day.MONDAY)
+        self.request.availability.create(day = Request.Day.WEDNESDAY)
+        self.request.availability.create(day = Request.Day.SUNDAY)
+    
+    def _assert_request_is_valid(self):
+        try:
+            self.request.full_clean()
+        except (ValidationError):
+            self.fail('Test request should be valid.')
+    
+    def _assert_request_is_invalid(self):
+        with self.assertRaises(ValidationError):
+            self.user.full_clean()
+    
+    def test_request_is_valid(self):
+        self._assert_request_is_valid()
+    
+    def test_user_must_exist(self):
+        self.request.user.delete()
+        self._assert_request_is_invalid()
+    
+    def test_availability_must_have_at_least_one_available_day(self):
+        self.request.availability.clear()
+        self._assert_request_is_invalid()
+    
+    def test_availability_can_have_one_available_day(self):
+        self.request.availability.clear()
+        self.request.availability.create(day = Request.Day.MONDAY)
+        self._assert_request_is_valid()
+
+    def test_availability_can_have_all_available_days(self):
+        self.request.availability.clear()
+        self.request.availability.create(day = Request.Day.MONDAY)
+        self.request.availability.create(day = Request.Day.TUESDAY)
+        self.request.availability.create(day = Request.Day.WEDNESDAY)
+        self.request.availability.create(day = Request.Day.THURSDAY)
+        self.request.availability.create(day = Request.Day.FRIDAY)
+        self.request.availability.create(day = Request.Day.SATURDAY)
+        self.request.availability.create(day = Request.Day.SUNDAY)
+        self._assert_request_is_valid()
+    
+    def test_number_of_lessons_cannot_be_below_one(self):
+        self.request.number_of_lessons = 0
+        self._assert_request_is_invalid()
+    
+    def test_further_information_cannot_be_blank(self):
+        self.request.further_information = ''
+        self._assert_request_is_invalid()
+    
+    def test_further_information_can_be_500_characters_long(self):
+        self.request.further_information = 'x' * 500
+        self._assert_request_is_valid()
+    
+    def test_further_information_cannot_be_over_500_characters_long(self):
+        self.request.further_information = 'x' * 501
+        self._assert_request_is_invalid()
