9f2e6f28fd76b6a0eba75690c4d972686a7ba6a0
Patryk Kugla <patryk.kugla@kcl.ac.uk>
2022-11-14 02:03:57 +0000

    Add "number_of_lessons" field to Booking model

--- lessons/models.py
+++ lessons/models.py
@@ -63,4 +63,5 @@ class Booking(models.Model):
     start_date = models.DateField(blank=False)
     duration_of_lessons = models.PositiveIntegerField(blank=False, choices=LessonDuration.choices)
     interval_between_lessons = models.PositiveIntegerField(choices=IntervalBetweenLessons.choices, blank=False)
+    number_of_lessons = models.PositiveIntegerField(blank=False)
     further_information = models.CharField(blank=False, max_length=500)
