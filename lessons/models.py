fd1cd560b037f02819a6cbbaf0218acc0572261e
absirgue <anton.sirgue@kcl.ac.uk>
2022-11-15 20:06:27 +0000

    Resolve migrations merge issues

--- lessons/models.py
+++ lessons/models.py
@@ -30,7 +30,6 @@ class User(AbstractBaseUser,PermissionsMixin):
     first_name = models.CharField(max_length = 50, blank=False, unique = False)
     last_name = models.CharField(max_length = 50, blank=False, unique = False)
     email = models.EmailField(unique = True, blank = False)
-    balance = models.IntegerField(default=0)
     is_active = models.BooleanField(
         ('active'),
         default=True,
