51e07e937855ee51cf3c699a8dd451fd9733028e
Patraiku <patrykkugla@gmail.com>
2022-11-19 01:16:02 +0000

    Remove commented out code

--- lessons/views.py
+++ lessons/views.py
@@ -34,6 +34,13 @@ def request_view(request):
     form = get_request_view_form(request)
     return render(request, 'request_view.html', {'date':date, 'form':form})
 
+@login_required
+@allowed_groups(['Student'])
+def request_view(request):
+    date = str(get_request_object(request).date)
+    form = get_request_view_form(request)
+    return render(request, 'request_view.html', {'date':date, 'form':form})
+
 
 def new_request_view(request):
     if request.method == 'POST':
@@ -119,45 +126,8 @@ def transaction_admin_view(request):
 # @allowed_groups(["Admin"])
 def admin_bookings_requests_view(request):
     if request.method == 'GET':
-
-        """
-        booking1 = Booking.objects.create(
-            student=User.objects.create_user(
-                email='jane2doe@gmail.com',
-                password="password"
-            ),
-            invoice_id="0002-001",
-            time_of_the_day="8:00",
-            teacher="Mr Smith",
-            number_of_lessons=15,
-            start_date="2022-11-22",
-            duration_of_lessons=Booking.LessonDuration.THIRTY_MINUTES,
-            interval_between_lessons=Booking.IntervalBetweenLessons.ONE_WEEK,
-            day_of_the_week=Booking.DayOfWeek.TUESDAY,
-            further_information="Extra Information"
-        )
-        booking1.save()
-        """
-
-        """
-        request1 = Request.objects.create(
-            user=User.objects.get(
-                email='email12@email.com'
-            ),
-            date=timezone.datetime(2000, 1, 1, 1, 1, 1, tzinfo=timezone.utc),
-            number_of_lessons=1,
-            interval_between_lessons=Request.IntervalBetweenLessons.ONE_WEEK,
-            duration_of_lessons=Request.LessonDuration.THIRTY_MINUTES,
-            further_information='Some information...'
-        )
-        """
         requests = Request.objects.all()
         bookings = Booking.objects.all()
-        # for request in requests:
-        # for duration in request.LessonDuration.choices:
-        # if duration[0] == request.duration_of_lessons:
-        # pass
-        # request.duration_of_lessons = duration[1]
         for req in requests:
             req.interval_between_lessons = req.IntervalBetweenLessons.choices[req.interval_between_lessons - 1][1]
             for duration in req.LessonDuration.choices:
