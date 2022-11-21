4b453971ed0b6a043290a7c1cca9739493bc6086
Quan Tran <quanabcis2003@gmail.com>
2022-11-21 17:48:49 +0000

    Add more tests to list views

--- lessons/tests/views/test_transaction_student_list.py
+++ lessons/tests/views/test_transaction_student_list.py
@@ -1,16 +1,50 @@
 from django.test import TestCase
 from django.urls import reverse
-from lessons.models import BankTransaction, User, Student
+from lessons.models import BankTransaction, User, Student, Invoice
 from django.db.models.query import QuerySet
+import datetime
 
 class TransactionAdminListTestCase(TestCase):
     def setUp(self):
         self.url = reverse('transaction_list_student')
-        self.user = User.objects.create_user(
+
+        self.user1 = User.objects.create_user(
                 email='email1@email.com',
                 password='password'
             )
-        self.student = Student.objects.create(user = self.user)
+
+        self.user2 = User.objects.create_user(
+                email='email2@email.com',
+                password='password'
+            )
+
+        self.student1 = Student.objects.create(user = self.user1)
+        self.student2 = Student.objects.create(user = self.user2)
+
+        self.invoice1 = Invoice.objects.create(
+            invoice_number='1234-123',
+            student=self.student1,
+            full_amount='100.00'
+        )
+        self.invoice2 = Invoice.objects.create(
+            invoice_number='1234-124',
+            student=self.student2,
+            full_amount='100.00'
+        )
+
+        self.transaction1 = BankTransaction.objects.create(
+            student = self.student1,
+            amount = '50.00',
+            invoice = self.invoice1,
+            date = datetime.date(2022, 2, 21)
+        )
+
+        self.transaction2 = BankTransaction.objects.create(
+            student = self.student2,
+            amount = '50.00',
+            invoice = self.invoice2,
+            date = datetime.date(2022, 2, 21)
+        )
 
     def test_transaction_admin_url(self):
         self.assertEqual(self.url, '/transactions/student')
@@ -26,10 +60,9 @@ class TransactionAdminListTestCase(TestCase):
     #TODO more tests for the transaction list view
 
     def test_transaction_student_displays_only_students_transactions(self):
-        pass
-
-    def test_transaction_student_displays_correct_fields(self):
-        pass
-
-    def test_transaction_student_displays_correct_data(self):
-        pass
+        self.client.login(email='email1@email.com', password='password')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'transaction_list.html')
+        transactions = response.context['transactions']
+        self.assertEqual(transactions.all().count(), BankTransaction.objects.filter(student=self.student1).count())
