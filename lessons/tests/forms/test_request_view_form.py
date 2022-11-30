a48df91f48e9b78a7e2fbcf5740555bbda2540bb
Yu Han Chen <yu.h.chen@kcl.ac.uk>
2022-11-30 09:25:36 +0000

    Add test for request view form

--- lessons/tests/forms/test_request_view_form.py
+++ lessons/tests/forms/test_request_view_form.py
@@ -1,8 +1,158 @@
+"""Unit tests of the request edit view"""
+from django.utils import timezone
 from django.test import TestCase
 from lessons.forms import RequestViewForm
-from lessons.tests.helpers import create_days_of_the_week
+from lessons.models import DayOfTheWeek, Request
+from django import forms
+from lessons.tests.helpers import create_days_of_the_week, HandleGroups
+
 
 class RequestViewFormTestCase(TestCase):
+    """Unit tests of the request edit view"""
+
+    fixtures = ['lessons/tests/fixtures/default_user.json']
+
     def setUp(self):
         create_days_of_the_week()
+        HandleGroups.set_default_user_to_student()
+
+        # Create form inputs
+        student_name = 'John Doe'
+        availability = DayOfTheWeek.objects.get(day=DayOfTheWeek.Day.TUESDAY), DayOfTheWeek.objects.get(
+            day=DayOfTheWeek.Day.WEDNESDAY)
+        number_of_lessons = 1
+        interval_between_lessons = Request.IntervalBetweenLessons.ONE_WEEK
+        duration_of_lessons = Request.LessonDuration.THIRTY_MINUTES
+        further_information = 'Some information'
+
+        # Also a fulfilled field?
+
+        self.form_input = {
+            'date': timezone.datetime.now(tz=timezone.utc),
+            'student_name': student_name,
+            'availability': availability,
+            'number_of_lessons': number_of_lessons,
+            'interval_between_lessons': interval_between_lessons,
+            'duration_of_lessons': duration_of_lessons,
+            'further_information': further_information,
+            'fulfilled': ''
+        }
+
+    def test_form_has_necessary_fields(self):
+        form = RequestViewForm()
+        self.assertIn('date', form.fields)
+        date_field = form.fields['date']
+        self.assertTrue(isinstance(date_field, forms.DateTimeField))
+
+        self.assertIn('availability', form.fields)
+
+        self.assertIn('number_of_lessons', form.fields)
+
+        self.assertIn('interval_between_lessons', form.fields)
+
+        self.assertIn('duration_of_lessons', form.fields)
+
+        self.assertIn('further_information', form.fields)
+        # Is fulfilled necessary here?
+
+    '''These tests require date field, but it is disabled in the form!
+    
+    def test_availability_cannot_accept_no_choices(self):
+        self.form_input['availability'] = None
+        form = RequestViewForm(data=self.form_input)
+        # form.fields['date'] = timezone.datetime.now(tz=timezone.utc)
+        self.assertFalse(form.is_valid())
+
+    def test_availability_can_accept_multiple_choices(self):
+        self.form_input['availability'] = DayOfTheWeek.objects.get(
+            day=DayOfTheWeek.Day.TUESDAY), DayOfTheWeek.objects.get(
+            day=DayOfTheWeek.Day.WEDNESDAY), DayOfTheWeek.objects.get(day=DayOfTheWeek.Day.THURSDAY)
+        form = RequestViewForm(data=self.form_input)
+        print(form)
+
+        self.assertTrue(form.is_valid())
+
+    def test_availability_can_accept_all_choices(self):
+        self.form_input['availability'] = DayOfTheWeek.objects.get(
+            day=DayOfTheWeek.Day.MONDAY), DayOfTheWeek.objects.get(
+            day=DayOfTheWeek.Day.TUESDAY), DayOfTheWeek.objects.get(
+            day=DayOfTheWeek.Day.WEDNESDAY), DayOfTheWeek.objects.get(
+            day=DayOfTheWeek.Day.THURSDAY), DayOfTheWeek.objects.get(day=DayOfTheWeek.Day.FRIDAY)
+        form = RequestViewForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+
+    def test_number_of_lessons_cannot_be_empty(self):
+        self.form_input['number_of_lessons'] = None
+        form = RequestViewForm(data=self.form_input)
+
+        self.assertFalse(form.is_valid())
+
+    def test_number_of_lessons_can_be_1(self):
+        self.form_input['number_of_lessons'] = 1
+        form = RequestViewForm(data=self.form_input)
+
+        self.assertTrue(form.is_valid())
+
+    def test_number_of_lessons_can_greater_than_1(self):
+        self.form_input['number_of_lessons'] = 2
+        form = RequestViewForm(data=self.form_input)
+
+        self.assertTrue(form.is_valid())
+
+    def test_number_of_lessons_cannot_be_less_than_1(self):
+        self.form_input['number_of_lessons'] = 0
+        form = RequestViewForm(data=self.form_input)
+        self.assertFalse(form.is_valid())
+
+    def test_number_of_lessons_can_be_max_int_for_SQLite_DB(self):
+        self.form_input['number_of_lessons'] = 9223372036854775807
+        form = RequestViewForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+
+    def test_number_of_lessons_cannot_be_greater_than_max_int_for_SQLite_DB(self):
+        self.form_input['number_of_lessons'] = 92233720368547758071
+        form = RequestViewForm(data=self.form_input)
+        self.assertFalse(form.is_valid())
+
+    def test_interval_between_lessons_cannot_be_empty(self):
+        self.form_input['interval_between_lessons'] = None
+        form = RequestViewForm(data=self.form_input)
+        self.assertFalse(form.is_valid())
+
+    def test_duration_of_lessons_can_accept_30_minutes(self):
+        self.form_input['duration_of_lessons'] = Request.LessonDuration.THIRTY_MINUTES
+        form = RequestViewForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+
+    def test_duration_of_lessons_can_accept_45_minutes(self):
+        self.form_input['duration_of_lessons'] = Request.LessonDuration.FOURTY_FIVE_MINUTES
+        form = RequestViewForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+
+    def test_duration_of_lessons_can_accept_60_minutes(self):
+        self.form_input['duration_of_lessons'] = Request.LessonDuration.SIXTY_MINUTES
+        form = RequestViewForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+
+    def test_duration_of_lessons_cannot_be_empty(self):
+        self.form_input['duration_of_lessons'] = None
+        form = RequestViewForm(data=self.form_input)
+        self.assertFalse(form.is_valid())
+
+    def test_further_information_cannot_be_empty(self):
+        self.form_input['further_information'] = ''
+        form = RequestViewForm(data=self.form_input)
+        self.assertFalse(form.is_valid())
+
+    def test_further_information_can_be_500_characters_long(self):
+        self.form_input['further_information'] = 'a' * 500
+        form = RequestViewForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+
+    def test_further_information_cannot_be_over_500_characters_long(self):
+        self.form_input['further_information'] = 'a' * 501
+        form = RequestViewForm(data=self.form_input)
+        self.assertFalse(form.is_valid())
         
+    '''
+    # Todo: add form_must_save_properly()
