d5c832b9793a9801a6f592f7ddbbb8a553423401
Yu Han Chen <yu.h.chen@kcl.ac.uk>
2022-12-04 18:11:26 +0000

    Change name of newRequestForm to RequestForm in request_form tests

--- lessons/tests/forms/test_request_view_form.py
+++ lessons/tests/forms/test_request_view_form.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from lessons.forms import RequestViewForm
+from lessons.forms import RequestForm
 from lessons.tests.helpers import create_days_of_the_week
 
 class RequestViewFormTestCase(TestCase):
