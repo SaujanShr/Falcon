114550398f92d7e26d130f063c579f72eac6f722
Patryk Kugla <patryk.kugla@kcl.ac.uk>
2022-12-08 06:09:25 +0000

    Remove unused functions for creating invoices

--- lessons/views_functions.py
+++ lessons/views_functions.py
@@ -394,40 +394,6 @@ def get_fulfil_request_form(request):
 def get_student(request):
     return Student.objects.get(user=request.user)
 
-def create_invoice(booking, hourly_cost):
-
-    user = Student.objects.get(user__email=booking.user)
-
-    invoice_number = generate_invoice_number(user)
-
-    #Calculate amount to pay
-    total_required = (float(hourly_cost) * booking.number_of_lessons * booking.duration_of_lessons / 60)
-
-    #Create invoice
-    invoice = Invoice.objects.create(
-        invoice_number=invoice_number,
-        student=user,
-        full_amount=total_required,
-        paid_amount=0,
-        fully_paid=False
-    )
-    invoice.full_clean()
-    invoice.save()
-
-    return invoice_number
-
-def generate_invoice_number(user):
-    invoice_number = ""
-    user_id = user.id
-    number_of_invoices_for_user = Invoice.objects.all().filter(student=user).count() + 1
-    for i in range(4-len(str(user_id))):
-        invoice_number += "0"
-    invoice_number = invoice_number + str(user_id) + "-"
-    for i in range(3-len(str(number_of_invoices_for_user))):
-        invoice_number += "0"
-    invoice_number += str(number_of_invoices_for_user)
-    return invoice_number
-
 def get_user(form):
     email = form.cleaned_data.get('email')
     password = form.cleaned_data.get('password')
