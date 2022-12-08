ab83f7bf07a1b079acd4c2df9b1f8954d28d556b
absirgue <anton.sirgue@kcl.ac.uk>
2022-12-08 11:38:36 +0000

    Add back in pythonanywhere required line

--- msms/settings.py
+++ msms/settings.py
@@ -16,7 +16,7 @@ from django.contrib.messages import constants as message_constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_ROOT = '/home/yc7/Falcon/staticfiles'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
